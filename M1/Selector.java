cb88f473611d1f90aba5f3319b4e4166cbe15e39
christinaadanks <christinaa.danks@me.com>
2021-03-20 01:02:45 -0700

    Add files via upload

---
+++ M1/Selector.java
@@ -0,0 +1,333 @@
+import java.util.Arrays;
+
+/**
+* Defines a library of selection methods
+* on arrays of ints.
+*
+* @author   Christinaa Danks
+* @author   Dean Hendrix 
+* @version  1/22/2021
+*
+*/
+public final class Selector {
+
+   /**
+    * Can't instantiate this class.
+    *
+    * D O   N O T   C H A N G E   T H I S   C O N S T R U C T O R
+    *
+    */
+   private Selector() { }
+
+
+   /**
+    * Selects the minimum value from the array a. This method
+    * throws IllegalArgumentException if a is null or has zero
+    * length. The array a is not changed by this method.
+    * @param a for Array.
+    * @return minimum value of array.
+    * @throws IllegalArgumentException for a == null or a.length == 0.
+    */
+   public static int min(int[] a) {
+      // exceptions
+      if (a == null || a.length == 0)  {
+         throw new IllegalArgumentException("Array cannot be null "
+            + "and must have 1 or more integers.");
+      }
+      
+      // select minimum value
+      int min = a[0];
+      for (int i = 0; i < a.length; i++)  {
+         if (a[i] < min)   {
+            min = a[i];
+         }
+      }
+      return min;
+   }
+
+
+   /**
+    * Selects the maximum value from the array a. This method
+    * throws IllegalArgumentException if a is null or has zero
+    * length. The array a is not changed by this method.
+    * @param a for Array.
+    * @return maximum value in array.
+    * @throws IllegalArgumentException for a == null or a.length == 0.
+    */
+   public static int max(int[] a)   {
+   
+      // exceptions
+      if (a == null || a.length == 0)  {
+         throw new IllegalArgumentException("Array cannot be null "
+            + "and must have 1 or more integers.");
+      }
+      
+      // select maximum value
+      int max = a[0];
+      for (int i = 0; i < a.length; i++)  {
+         if (a[i] > max)   {
+            max = a[i];
+         }
+      }
+      return max;
+   }
+
+
+   /**
+    * Selects the kth minimum value from the array a. This method
+    * throws IllegalArgumentException if a is null, has zero length,
+    * or if there is no kth minimum value. Note that there is no kth
+    * minimum value if k < 1, k > a.length, or if k is larger than
+    * the number of distinct values in the array. The array a is not
+    * changed by this method.
+    * @param a for Array.
+    * @param k for k-1 distinct values less than kmin in array.
+    * @return kth minimum value.
+    * @throws IllegalArgumentException for a == null, 
+    * a.length == 0, or no kth min value.
+    */
+   public static int kmin(int[] a, int k) {
+   
+      // exceptions
+      if (a == null || a.length == 0)  {
+         throw new IllegalArgumentException("Array cannot be null "
+            + "and must have 1 or more integers.");
+      }
+      if (k < 1 || k > a.length) {
+         throw new IllegalArgumentException("There is no kth "
+         + "minimum value in array.");
+      }
+      
+      // copy & sort array
+      int[] b = Arrays.copyOf(a, a.length);
+      Arrays.sort(b);
+      
+      // solve for kmin. 
+      int last = b.length - 1;
+      int uniqueNum = 1;
+      int kmin = 0;
+      if (k == 1) {
+         kmin = min(a);
+      }
+      for (int i = 0; i < last; i++)  {
+         if (b[i] != b[i + 1])   {
+            uniqueNum++;
+            if (uniqueNum == k)  {
+               kmin = b[i + 1]; // based off mapped calculations
+            } 
+         }
+      }
+      
+      // exceptions
+      if (k > uniqueNum)   {
+         throw new IllegalArgumentException("k must be less "
+            + "than or equal to the number of distinct integers.");
+      }
+      return kmin;
+   }
+
+
+   /**
+    * Selects the kth maximum value from the array a. This method
+    * throws IllegalArgumentException if a is null, has zero length,
+    * or if there is no kth maximum value. Note that there is no kth
+    * maximum value if k < 1, k > a.length, or if k is larger than
+    * the number of distinct values in the array. The array a is not
+    * changed by this method.
+    * @param a for Array.
+    * @param k for k-1 distinct values greater than kmax in array.
+    * @return kth maximum value.
+    * @throws IllegalArgumentException for a == null, 
+    * a.length == 0, or no kth max value.
+    */
+   public static int kmax(int[] a, int k) {
+   
+      // exceptions
+      if (a == null || a.length < 0)   {
+         throw new IllegalArgumentException("Array cannot be null "
+            + "and must have 1 or more integers.");
+      }
+      if (k < 1 || k > a.length) {
+         throw new IllegalArgumentException("There is no kth "
+            + "maximum value in array.");
+      }
+      
+      // copy & sort array
+      int[] b = Arrays.copyOf(a, a.length);
+      Arrays.sort(b);
+      
+      // solve for kmax
+      int last = b.length - 1;
+      int uniqueNum = 1;
+      int kmax = 0;
+      if (k == 1)   {
+         kmax = max(a);
+      }
+      
+      for (int i = last; i > 0; i--)   {
+         if (b[i] != b[i - 1])   {
+            uniqueNum++;
+            if (uniqueNum == k)  {
+               kmax = b[i - 1];
+            }
+         }
+      }      
+      
+      // exceptions
+      if (k > uniqueNum)   {
+         throw new IllegalArgumentException("k must be less "
+            + "than or equal to the number of distinct integers.");
+      }      
+      return kmax;
+   }
+
+
+   /**
+    * Returns an array containing all the values in a in the
+    * range [low..high]; that is, all the values that are greater
+    * than or equal to low and less than or equal to high,
+    * including duplicate values. The length of the returned array
+    * is the same as the number of values in the range [low..high].
+    * If there are no qualifying values, this method returns a
+    * zero-length array. Note that low and high do not have
+    * to be actual values in a. This method throws an
+    * IllegalArgumentException if a is null or has zero length.
+    * The array a is not changed by this method.
+    * @param a for Array.
+    * @param low for low point of range.
+    * @param high for high point of range.
+    * @return range asn array of low and high values.
+    * @throws IllegalArgumentException for a == null or a.length == 0.
+    */
+   public static int[] range(int[] a, int low, int high) {
+   
+      // exceptions
+      if (a == null || a.length == 0)  {
+         throw new IllegalArgumentException("Array cannot be null "
+            + "and must have 1 or more integers.");
+      }
+            
+      // get length of range array
+      int rLength = 0;
+      for (int i = 0; i < a.length; i++)  {
+         if (a[i] >= low && a[i] <= high) {
+            rLength++;
+         }
+      }
+      
+      // create new range array
+      int[] range = new int[rLength];
+      
+      // zero length array
+      if (rLength == 0) {
+         return range;
+      }
+      
+      // range array
+      int r = 0;
+      for (int i = 0; i < a.length; i++)  {
+         if (a[i] >= low && a[i] <= high) {
+            range[r] = a[i];
+            r++;
+         }
+      }    
+      return range;
+   }
+
+
+   /**
+    * Returns the smallest value in a that is greater than or equal to
+    * the given key. This method throws an IllegalArgumentException if
+    * a is null or has zero length, or if there is no qualifying
+    * value. Note that key does not have to be an actual value in a.
+    * The array a is not changed by this method.
+    * @param a for Array.
+    * @param key for integer to find ceiling of.
+    * @return ceiling of key.
+    * @throws IllegalArgumentException for a == null, 
+    * a.length == 0, or no ceiling.
+    */
+   public static int ceiling(int[] a, int key) {
+   
+      // exceptions
+      if (a == null || a.length == 0)  {
+         throw new IllegalArgumentException("Array cannot be null "
+            + "and must have 1 or more integers.");
+      }
+      
+      // get length of ceiling array
+      int cLength = 0;
+      for (int i = 0; i < a.length; i++)  {
+         if (a[i] >= key)  {
+            cLength++;
+         }
+      }
+      
+      // create new ceiling array
+      int[] ceiling = new int[cLength];
+      
+      // exceptions
+      if (cLength == 0) {
+         throw new IllegalArgumentException("No ceiling value found.");
+      }
+      
+      // add values to ceiling array & return minimum value of ceiling array
+      int c = 0;
+      for (int i = 0; i < a.length; i++)  {
+         if (a[i] >= key)  {
+            ceiling[c] = a[i];
+            c++;
+         }
+      }
+      return min(ceiling);
+   }
+
+
+   /**
+    * Returns the largest value in a that is less than or equal to
+    * the given key. This method throws an IllegalArgumentException if
+    * a is null or has zero length, or if there is no qualifying
+    * value. Note that key does not have to be an actual value in a.
+    * The array a is not changed by this method.
+    * @param a for Array.
+    * @param key for integer to find floor of.
+    * @return floor of key.
+    * @throws IllegalArgumentException for a == null, 
+    * a.length == 0, or no floor.
+    */
+   public static int floor(int[] a, int key) {
+   
+      // exceptions
+      if (a == null || a.length == 0)  {
+         throw new IllegalArgumentException("Array cannot be null "
+            + "and must have 1 or more integers.");
+      }
+      
+      // get length of floor array
+      int fLength = 0;
+      for (int i = 0; i < a.length; i++)  {
+         if (a[i] <= key)  {
+            fLength++;
+         }
+      }
+      
+      // create new floor array
+      int[] floor = new int[fLength];
+      
+      // exception
+      if (fLength == 0) {
+         throw new IllegalArgumentException("No floor value found.");
+      }
+      
+      // add values to floor array & return maximum value of floor array
+      int f = 0;
+      for (int i = 0; i < a.length; i++)  {
+         if (a[i] <= key)  {
+            floor[f] = a[i];
+            f++;
+         }
+      }
+      return max(floor);
+   }
+
+}
---
+++ M1/SelectorTest.java
@@ -0,0 +1,641 @@
+import org.junit.Assert;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Christinaa Danks
+ * @version 1/22/2021
+ */
+ 
+public class SelectorTest {
+
+   /////////////////////////////////
+   ///// Tests for min method. /////
+   /////////////////////////////////
+   
+   /**
+    * Tests for illegal cases.
+    */
+   @Test
+   public void minExceptionTest()   {
+   
+      int[] a = new int[0];
+      try   {
+         Selector.min(a);
+         Assert.fail("Did not throw IllegalArgumentException.");
+      }
+      catch (IllegalArgumentException e)  {
+         Assert.assertTrue(true);
+      }
+      catch (Exception e)  {
+         Assert.fail("Threw incorrect exception.");
+      }
+   }
+   
+   @Test
+   public void minExceptionTest2()  {
+   
+      int[] a = null;
+      try   {
+         Selector.min(a);
+         Assert.fail("Did not throw IllegalArgumentException.");
+      }
+      catch (IllegalArgumentException e)  {
+         Assert.assertTrue(true);
+      }
+      catch (Exception e)  {
+         Assert.fail("Threw incorrect exception.");
+      }
+   }
+   
+   /**
+    * Tests for typical cases.
+    */
+   @Test
+   public void minNormTest()  {
+      int[] a = {2, 8, 7, 3, 4};
+      Assert.assertEquals(2, Selector.min(a));
+   }
+
+   @Test
+   public void minNormTest2()  {
+      int[] a = {8, 7, 6, 5, 4};
+      Assert.assertEquals(4, Selector.min(a));
+   }
+   
+   /**
+    * Tests for boundary cases.
+    */
+   @Test
+   public void minBoundTest() {
+      int[] a = {0, 3};
+      Assert.assertEquals(0, Selector.min(a));
+   }
+   
+   /**
+    * Tests for special cases.
+    */
+   @Test
+   public void minSpecialTest() {
+      int[] a = {8, 2, 8, 7, 3, 3, 4};
+      Assert.assertEquals(2, Selector.min(a));
+   }
+   
+   /////////////////////////////////
+   ///// Tests for max method. /////
+   /////////////////////////////////
+   
+   /**
+    * Tests for illegal cases.
+    */
+   @Test
+   public void maxIllegalTest()  {
+      
+      int[] a = new int[0];
+      try   {
+         Selector.max(a);
+         Assert.fail("Did not throw IllegalArgumentException.");
+      }
+      catch (IllegalArgumentException e)  {
+         Assert.assertTrue(true);
+      }
+      catch (Exception e)  {
+         Assert.fail("Threw incorrect exception.");
+      }
+   }
+   
+   @Test
+   public void maxIllegalTest2() {
+   
+      int[] a = null;
+      try {
+         Selector.max(a);
+         Assert.fail("Did not throw IllegalArgumentException.");
+      }
+      catch (IllegalArgumentException e)  {
+         Assert.assertTrue(true);
+      }
+      catch (Exception e)  {
+         Assert.fail("Threw incorrect exception.");
+      }
+   }
+   
+   
+   /**
+    * Tests for typical cases.
+    */
+   @Test
+   public void maxNormTest()  {
+      int[] a = {2, 8, 7, 3, 4};
+      Assert.assertEquals(8, Selector.max(a));
+   }
+
+   @Test
+   public void maxNormTest2()  {
+      int[] a = {8, 7, 6, 5, 4};
+      Assert.assertEquals(8, Selector.max(a));
+   }
+   
+   /**
+    * Tests for boundary cases.
+    */
+   @Test
+   public void maxBoundTest() {
+      int[] a = {0, 3};
+      Assert.assertEquals(3, Selector.max(a));
+   }
+   
+   /**
+    * Tests for special cases.
+    */
+   @Test
+   public void maxSpecialTest() {
+      int[] a = {8, 2, 8, 7, 3, 3, 4};
+      Assert.assertEquals(8, Selector.max(a));
+   }
+   
+   /////////////////////////////////
+   ///// Tests for kmin method. /////
+   /////////////////////////////////
+   
+   /**
+    * Tests for illegal cases.
+    */
+   @Test
+   public void kminIllegalTest()  {
+      
+      int[] a = new int[0];
+      try   {
+         Selector.kmin(a, 1);
+         Assert.fail("Did not throw IllegalArgumentException.");
+      }
+      catch (IllegalArgumentException e)  {
+         Assert.assertTrue(true);
+      }
+      catch (Exception e)  {
+         Assert.fail("Threw incorrect exception.");
+      }
+   }
+   
+   @Test
+   public void kminIllegalTest2() {
+   
+      int[] a = null;
+      try {
+         Selector.kmin(a, 1);
+         Assert.fail("Did not throw IllegalArgumentException.");
+      }
+      catch (IllegalArgumentException e)  {
+         Assert.assertTrue(true);
+      }
+      catch (Exception e)  {
+         Assert.fail("Threw incorrect exception.");
+      }
+   }
+   
+   @Test
+   public void kminIllegalTest3() {
+   
+      int[] a = {2, 6, 7, 9};
+      try {
+         Selector.kmin(a, 5);
+         Assert.fail("Did not throw IllegalArgumentException.");
+      }
+      catch (IllegalArgumentException e)  {
+         Assert.assertTrue(true);
+      }
+      catch (Exception e)  {
+         Assert.fail("Threw incorrect exception.");
+      }
+   }
+   
+   
+   /**
+    * Tests for typical cases.
+    */
+   @Test
+   public void kminNormTest()  {
+      int[] a = {2, 8, 7, 3, 4};
+      Assert.assertEquals(2, Selector.kmin(a, 1));
+   }
+
+   @Test
+   public void kminNormTest2()  {
+      int[] a = {5, 9, 1, 7, 3};
+      Assert.assertEquals(5, Selector.kmin(a, 3));
+   }
+   
+   @Test
+   public void kminNormTest3()  {
+      int[] a = {2, 6, 1, 8};
+      Assert.assertEquals(8, Selector.kmin(a, 4));
+   }
+   
+
+   /**
+    * Tests for special cases.
+    */
+   @Test
+   public void kminSpecialTest() {
+      int[] a = {8, 2, 8, 7, 3, 3, 4};
+      Assert.assertEquals(4, Selector.kmin(a, 3));
+   }
+   
+   @Test
+   public void kminSpecialTest2() {
+      int[] a = {5, 9, 7, 3, 1, 3};
+      Assert.assertEquals(3, Selector.kmin(a, 2));
+   }
+   
+   @Test
+   public void kminSpecialTest3() {
+      int[] a = {3, 1, 5, 1, 9, 7, 7};
+      Assert.assertEquals(9, Selector.kmin(a, 5));
+   }
+   
+   @Test
+   public void kminSpecialTest4() {
+      int[] a = {3, 1, 1, 5, 5, 7, 1, 5};
+      Assert.assertEquals(7, Selector.kmin(a, 4));
+   }   
+   
+   @Test
+   public void kminSpecialTest5() {
+      int[] a = {7, 1, 0, 6};
+      Assert.assertEquals(0, Selector.kmin(a, 1));
+   }   
+   
+   ////////////////////////////////
+   //// Tests for kmax method. ////
+   ////////////////////////////////
+   
+   /**
+    * Tests for illegal cases.
+    */
+   @Test
+   public void kmaxIllegalTest()  {
+      
+      int[] a = new int[0];
+      try   {
+         Selector.kmax(a, 1);
+         Assert.fail("Did not throw IllegalArgumentException.");
+      }
+      catch (IllegalArgumentException e)  {
+         Assert.assertTrue(true);
+      }
+      catch (Exception e)  {
+         Assert.fail("Threw incorrect exception.");
+      }
+   }
+   
+   @Test
+   public void kmaxIllegalTest2() {
+   
+      int[] a = null;
+      try {
+         Selector.kmax(a, 1);
+         Assert.fail("Did not throw IllegalArgumentException.");
+      }
+      catch (IllegalArgumentException e)  {
+         Assert.assertTrue(true);
+      }
+      catch (Exception e)  {
+         Assert.fail("Threw incorrect exception.");
+      }
+   }
+   
+   @Test
+   public void kmaxIllegalTest3() {
+   
+      int[] a = {2, 6, 7, 9};
+      try {
+         Selector.kmax(a, 5);
+         Assert.fail("Did not throw IllegalArgumentException.");
+      }
+      catch (IllegalArgumentException e)  {
+         Assert.assertTrue(true);
+      }
+      catch (Exception e)  {
+         Assert.fail("Threw incorrect exception.");
+      }
+   }
+   
+   
+   /**
+    * Tests for typical cases.
+    */
+   @Test
+   public void kmaxNormTest()  {
+      int[] a = {2, 8, 7, 3, 4};
+      Assert.assertEquals(8, Selector.kmax(a, 1));
+   }
+
+   @Test
+   public void kmaxNormTest2()  {
+      int[] a = {5, 9, 1, 7, 3};
+      Assert.assertEquals(5, Selector.kmax(a, 3));
+   }
+   
+   @Test
+   public void kmaxNormTest3()  {
+      int[] a = {2, 6, 1, 8};
+      Assert.assertEquals(1, Selector.kmax(a, 4));
+   }
+   
+
+   /**
+    * Tests for special cases.
+    */
+   @Test
+   public void kmaxSpecialTest() {
+      int[] a = {8, 2, 8, 7, 3, 3, 4};
+      Assert.assertEquals(4, Selector.kmax(a, 3));
+   }
+   
+   @Test
+   public void kmaxSpecialTest2() {
+      int[] a = {5, 9, 7, 3, 1, 3};
+      Assert.assertEquals(1, Selector.kmax(a, 5));
+   }
+   
+   @Test
+   public void kmaxSpecialTest3() {
+      int[] a = {3, 1, 5, 1, 9, 7, 7};
+      Assert.assertEquals(3, Selector.kmax(a, 4));
+   }
+   
+   @Test
+   public void kmaxSpecialTest4() {
+      int[] a = {3, 2, 1, 7, 7, 5, 5, 7, 5};
+      Assert.assertEquals(2, Selector.kmax(a, 4));
+   }   
+   
+   @Test
+   public void kmaxSpecialTest5() {
+      int[] a = {7, 1, 0, 6};
+      Assert.assertEquals(7, Selector.kmax(a, 1));
+   }   
+   
+   /////////////////////////////////
+   //// Tests for range method. ////
+   /////////////////////////////////
+   
+   /**
+    * Tests for illegal cases.
+    */
+   @Test
+   public void rangeIllegalTest()  {
+      
+      int[] a = new int[0];
+      try   {
+         Selector.range(a, 1, 6);
+         Assert.fail("Did not throw IllegalArgumentException.");
+      }
+      catch (IllegalArgumentException e)  {
+         Assert.assertTrue(true);
+      }
+      catch (Exception e)  {
+         Assert.fail("Threw incorrect exception.");
+      }
+   }
+   
+   @Test
+   public void rangeIllegalTest2() {
+   
+      int[] a = null;
+      try {
+         Selector.range(a, 1, 6);
+         Assert.fail("Did not throw IllegalArgumentException.");
+      }
+      catch (IllegalArgumentException e)  {
+         Assert.assertTrue(true);
+      }
+      catch (Exception e)  {
+         Assert.fail("Threw incorrect exception.");
+      }
+   }
+   
+   /**
+    * Tests for typical cases.
+    */
+   @Test
+   public void rangeNormTest()  {
+      int[] a = {2, 8, 7, 3, 4};
+      int[] expected = {2, 3, 4};
+      int low = 1;
+      int high = 5;
+      Assert.assertArrayEquals(expected, Selector.range(a, low, high));
+   }
+
+   @Test
+   public void rangeNormTest2()  {
+      int[] a = {5, 9, 1, 7, 3};
+      int[] expected = {5, 3};
+      int low = 3;
+      int high = 5;
+      Assert.assertArrayEquals(expected, Selector.range(a, low, high));
+   }
+   
+   @Test
+   public void rangeNormTest3()  {
+      int[] a = {8, 7, 6, 5, 4};
+      int[] expected = {8, 7, 6, 5, 4};
+      int low = 4;
+      int high = 8;
+      Assert.assertArrayEquals(expected, Selector.range(a, low, high));
+   }
+   
+
+   /**
+    * Tests for special cases.
+    */
+   @Test
+   public void rangeSpecialTest() {
+      int[] a = {8, 2, 8, 7, 3, 3, 4};
+      int[] expected = {7, 3, 3, 4};
+      int low = 3;
+      int high = 7;
+      Assert.assertArrayEquals(expected, Selector.range(a, low, high));
+   }
+   
+   ///////////////////////////////////
+   //// Tests for ceiling method. ////
+   ///////////////////////////////////
+   
+   /**
+    * Tests for illegal cases.
+    */
+   @Test
+   public void ceilingIllegalTest()  {
+      
+      int[] a = new int[0];
+      try   {
+         Selector.ceiling(a, 5);
+         Assert.fail("Did not throw IllegalArgumentException.");
+      }
+      catch (IllegalArgumentException e)  {
+         Assert.assertTrue(true);
+      }
+      catch (Exception e)  {
+         Assert.fail("Threw incorrect exception.");
+      }
+   }
+   
+   @Test
+   public void ceilingIllegalTest2() {
+   
+      int[] a = null;
+      try {
+         Selector.ceiling(a, 3);
+         Assert.fail("Did not throw IllegalArgumentException.");
+      }
+      catch (IllegalArgumentException e)  {
+         Assert.assertTrue(true);
+      }
+      catch (Exception e)  {
+         Assert.fail("Threw incorrect exception.");
+      }
+   }
+   
+   @Test
+   public void ceilingIllegalTest3()   {
+   
+      int[] a = {8, 7, 6, 4};
+      try   {
+         Selector.ceiling(a, 9);
+         Assert.fail("Did not throw IllegalArgumentException.");
+      }
+      catch (IllegalArgumentException e)  {
+         Assert.assertTrue(true);
+      }
+      catch (Exception e)  {
+         Assert.fail("Threw incorrect exception.");
+      }
+   }
+   
+   /**
+    * Tests for typical cases.
+    */
+   @Test
+   public void ceilingNormTest()  {
+      int[] a = {2, 8, 7, 3, 4};
+      
+      Assert.assertEquals(2, Selector.ceiling(a, 1));
+   }
+
+   @Test
+   public void ceilingNormTest2()  {
+      int[] a = {2, 8, 7, 3, 4};
+      
+      Assert.assertEquals(7, Selector.ceiling(a, 7));
+   }
+   
+   @Test
+   public void ceilingNormTest3()  {
+      int[] a = {8, 7, 6, 5, 4};
+      
+      Assert.assertEquals(4, Selector.ceiling(a, 0));
+   }
+   
+
+   /**
+    * Tests for special cases.
+    */
+   @Test
+   public void ceilingSpecialTest() {
+      int[] a = {8, 2, 8, 7, 3, 3, 4};
+      
+      Assert.assertEquals(7, Selector.ceiling(a, 5));
+   }      
+   
+   /////////////////////////////////
+   //// Tests for floor method. ////
+   /////////////////////////////////
+   
+   /**
+    * Tests for illegal cases.
+    */
+   @Test
+   public void floorIllegalTest()  {
+      
+      int[] a = new int[0];
+      try   {
+         Selector.floor(a, 5);
+         Assert.fail("Did not throw IllegalArgumentException.");
+      }
+      catch (IllegalArgumentException e)  {
+         Assert.assertTrue(true);
+      }
+      catch (Exception e)  {
+         Assert.fail("Threw incorrect exception.");
+      }
+   }
+   
+   @Test
+   public void floorIllegalTest2() {
+   
+      int[] a = null;
+      try {
+         Selector.floor(a, 3);
+         Assert.fail("Did not throw IllegalArgumentException.");
+      }
+      catch (IllegalArgumentException e)  {
+         Assert.assertTrue(true);
+      }
+      catch (Exception e)  {
+         Assert.fail("Threw incorrect exception.");
+      }
+   }
+   
+   @Test
+   public void floorIllegalTest3()   {
+   
+      int[] a = {8, 7, 6, 4};
+      try   {
+         Selector.floor(a, 0);
+         Assert.fail("Did not throw IllegalArgumentException.");
+      }
+      catch (IllegalArgumentException e)  {
+         Assert.assertTrue(true);
+      }
+      catch (Exception e)  {
+         Assert.fail("Threw incorrect exception.");
+      }
+   }
+   
+   /**
+    * Tests for typical cases.
+    */
+   @Test
+   public void floorNormTest()  {
+      int[] a = {2, 8, 7, 3, 4};
+      
+      Assert.assertEquals(4, Selector.floor(a, 6));
+   }
+
+   @Test
+   public void floorNormTest2()  {
+      int[] a = {5, 9, 1, 7, 3};
+      
+      Assert.assertEquals(1, Selector.floor(a, 1));
+   }
+   
+   @Test
+   public void floorNormTest3()  {
+      int[] a = {8, 7, 6, 5, 4};
+      
+      Assert.assertEquals(8, Selector.floor(a, 9));
+   }
+   
+
+   /**
+    * Tests for special cases.
+    */
+   @Test
+   public void floorSpecialTest() {
+      int[] a = {8, 2, 8, 7, 3, 3, 4};
+      
+      Assert.assertEquals(4, Selector.floor(a, 5));
+   }         
+}
